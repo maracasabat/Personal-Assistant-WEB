e4692b4f60860672ac676617b2af956695ae5ee0
Aleksandra0001 <104300297+Aleksandra0001@users.noreply.github.com>
2022-11-12 19:17:22 +0200

    add admin config, HTMLfield for admin, models user, models post
    add django-tinymce

--- assistant/assistant/settings.py
+++ assistant/assistant/settings.py
@@ -42,8 +42,12 @@ INSTALLED_APPS = [
     'tailwind',
     'theme',
     'django_browser_reload',
-    'main',
+    'main.apps.MainConfig',
+    'users.apps.UsersConfig',
+    'tinymce',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -85,6 +89,17 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'testdb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'password',
+#         'HOST': '45.76.42.234',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -136,3 +151,25 @@ INTERNAL_IPS = [
 NPM_BIN_PATH = '/usr/local/bin/npm'  # MacOS
 
 # NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd" # Windows
+
+#  Configuring TinyMCE for admin
+
+TINYMCE_DEFAULT_CONFIG = {
+    'custom_undo_redo_levels': 100,
+    'selector': 'textarea',
+    "menubar": "file edit view insert format tools table help",
+    'plugins': 'link image preview codesample contextmenu table code lists fullscreen',
+    'toolbar1': 'undo redo | backcolor casechange permanentpen formatpainter removeformat formatselect fontselect fontsizeselect',
+    'toolbar2': 'bold italic underline blockquote | alignleft aligncenter alignright alignjustify '
+               '| bullist numlist | outdent indent | table | link image | codesample | preview code | tiny_mce_wiris_formulaEditor tiny_mce_wiris_formulaEditorChemistry',
+    'contextmenu': 'formats | link image',
+    'block_formats': 'Paragraph=p; Header 1=h1; Header 2=h2',
+    'fontsize_formats': "8pt 10pt 12pt 14pt 16pt 18pt",
+    'content_style': "body { font-family: Arial; background: white; color: black; font-size: 12pt}",
+    'codesample_languages': [
+        {'text': 'Python', 'value': 'python'}, {'text': 'HTML/XML', 'value': 'markup'},],
+    'image_class_list': [{'title': 'Fluid', 'value': 'img-fluid', 'style': {} }],
+    'width': 'auto',
+    "height": "600px",
+    'image_caption': True,
+}
--- assistant/main/views.py
+++ assistant/main/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
+def homepage(request):
     return render(request, "main/index.html")
