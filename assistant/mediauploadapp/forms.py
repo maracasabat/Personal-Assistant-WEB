f5c37318d671d1aa08b7340c071f73e99afb00c9
kovalch <nataliia.kovalchuk@liip.ch>
2022-11-13 07:44:15 +0200

    feat: Add limitations on file size and file type

--- assistant/mediauploadapp/forms.py
+++ assistant/mediauploadapp/forms.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from .models import Book
+from .filechecker import file_size, file_extension
 
 class BookForm(forms.ModelForm):
+    pdf = forms.FileField(required=True, validators=[file_size, file_extension])
     class Meta:
         model = Book
         fields = ('title', 'author', 'pdf', 'cover')
--- assistant/mediauploadapp/models.py
+++ assistant/mediauploadapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from .filechecker import file_size, file_extension
 
 # Create your models here.
 
