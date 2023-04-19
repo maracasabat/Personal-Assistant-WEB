cb0c0f74d505d278862bfea0f3820f5c3ad77d66
Ivan Yevpat <maracasabat@gmail.com>
2023-04-19 21:21:21 +0300

    fixed newsapp

--- assistant/newsapp/views.py
+++ assistant/newsapp/views.py
@@ -15,17 +15,40 @@ import requests
 def get_news(request):
     news = []
     base_url = "https://www.pravda.com.ua/news/"
-    response = requests.get(base_url)
+
+    # Get response from server
+    try:
+        response = requests.get(base_url)
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return render(request, 'error.html', {'message': 'Could not get response from server.'})
+
     soup = BeautifulSoup(response.text, 'html.parser')
-    content = soup.select('div[class="container_sub_news_list_wrapper mode1"] div[class="article_news_list"]')
-    # print(content)
+
+    # Parse content
+    try:
+        content = soup.select('div[class="container_sub_news_list_wrapper mode1"] div[class="article_news_list"]')
+    except AttributeError as e:
+        print(f"Error: {e}")
+        return render(request, 'error.html', {'message': 'Could not parse content.'})
+
+    # Get news items
     for el in content:
         result = {}
-        result['time'] = el.find('div', {'class': 'article_time'}).text
-        result['title'] = el.find('div', {'class': 'article_header'}).text
-        # news['link'] = el.find_next('a').get('href')
+
+        try:
+            result['time'] = el.find('div', {'class': 'article_time'}).text
+            result['title'] = el.find('div', {'class': 'article_header'}).text
+        except AttributeError as e:
+            print(f"Error: {e}")
+            continue
+
         news.append(result)
-        # print(news)
+
+    # Check if any news found
+    if not news:
+        return render(request, 'error.html', {'message': 'No news found.'})
+
     return render(request, 'scrape.html', {'news': news})
 
 
@@ -33,17 +56,40 @@ def get_news(request):
 def get_sport_news(request):
     sport_news = []
     base_url = "https://sport.ua/uk/uk"
-    response = requests.get(base_url)
+    # Get response from server
+    try:
+        response = requests.get(base_url)
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return render(request, 'error.html', {'message': 'Could not get response from server.'})
+
     soup = BeautifulSoup(response.text, 'html.parser')
-    content = soup.find('div', class_='news-items').find_all('div', class_='item')
-    # print(content)
+
+    # Parse content
+    try:
+        content = soup.find('div', class_='news-items').find_all('div', class_='item')
+    except AttributeError as e:
+        print(f"Error: {e}")
+        return render(request, 'error.html', {'message': 'Could not parse content.'})
+
+    # Get news
     for el in content:
         result = {}
-        result['time'] = el.find('span', class_='item-date').text.strip()
-        result['sport'] = el.find('span', class_='item-sport').text.casefold()
-        result['news'] = el.find('div', {'class': 'item-title'}).text.strip()
+
+        try:
+            result['time'] = el.find('span', class_='item-date').text.strip()
+            result['sport'] = el.find('span', class_='item-sport').text.casefold()
+            result['news'] = el.find('div', {'class': 'item-title'}).text.strip()
+        except AttributeError as e:
+            print(f"Error: {e}")
+            continue
+
         sport_news.append(result)
-        # print(sport_news)
+
+        # Check if any news found
+    if not sport_news:
+        return render(request, 'error.html', {'message': 'No news found.'})
+
     return render(request, 'scrape_sport.html', {'sport_news': sport_news})
 
 
@@ -52,22 +98,44 @@ def get_currency(request):
     currency = []
     date_now = datetime.now().strftime('%Y-%m-%d')
     base_url = 'https://minfin.com.ua/ua/currency/banks/usd/'
-    response = requests.get(base_url + date_now + '/')
+
+    # Get response from server
+    try:
+        response = requests.get(base_url + date_now + '/')
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return render(request, 'error.html', {'message': 'Could not get response from server.'})
+
     soup = BeautifulSoup(response.text, 'html.parser')
-    content = soup.find('tbody', class_='list').find_all('tr')
-    # print(content)
+
+    # Parse content
+    try:
+        content = soup.find('tbody', class_='list').find_all('tr')
+    except AttributeError as e:
+        print(f"Error: {e}")
+        return render(request, 'error.html', {'message': 'Could not parse content.'})
+
+    # Get news
     for el in content:
         result = {}
-        result['bank'] = el.find('a', {'class': 'mfm-black-link'}).text.strip()
-        result['buy'] = el.find('td', {'class': 'responsive-hide mfm-text-right mfm-pr0'}).text.strip()
-        if len(result['buy']) == 0:
-            result['buy'] = '0.000'
-        result['sale'] = el.find('td', {'class': 'responsive-hide mfm-text-left mfm-pl0'}).text.strip()
-        if len(result['sale']) == 0:
-            result['sale'] = '0.000'
-        result['date'] = el.find('td', {'class': 'respons-collapsed mfcur-table-refreshtime'}).text.strip()
+
+        try:
+            result['bank'] = el.find('a', {'class': 'mfm-black-link'}).text.strip()
+            result['buy'] = el.find('td', {'class': 'responsive-hide mfm-text-right mfm-pr0'}).text.strip()
+            if len(result['buy']) == 0:
+                result['buy'] = '0.000'
+            result['sale'] = el.find('td', {'class': 'responsive-hide mfm-text-left mfm-pl0'}).text.strip()
+            if len(result['sale']) == 0:
+                result['sale'] = '0.000'
+            result['date'] = el.find('td', {'class': 'respons-collapsed mfcur-table-refreshtime'}).text.strip()
+        except AttributeError as e:
+            print(f"Error: {e}")
+            continue
 
         currency.append(result)
+    # Check if any news found
+    if not currency:
+        return render(request, 'error.html', {'message': 'No news found.'})
     return render(request, 'scrape_currency.html', {'currency': currency})
 
 
@@ -76,17 +144,42 @@ def get_it(request):
     it = []
     base_url = 'https://focus.ua/uk/technologies/list'
     urls = ['/', '?page=2', '?page=3', '?page=4', '?page=5', '?page=6', '?page=7', '?page=8', '?page=9']
+
+    # Get news from each URL
     for url in urls:
-        response = requests.get(base_url + url)
+        # Get response from server
+        try:
+            response = requests.get(base_url + url)
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+            return render(request, 'error.html', {'message': 'Could not get response from server.'})
+
         soup = BeautifulSoup(response.text, 'html.parser')
-        content = soup.select('div[class="c-card-list__main"]')
-        # print(content)
+
+        # Parse content
+        try:
+            content = soup.select('div[class="c-card-list__main"]')
+        except AttributeError as e:
+            print(f"Error: {e}")
+            return render(request, 'error.html', {'message': 'Could not parse content.'})
+
+        # Get news items
         for el in content:
             result = {}
-            result['title'] = el.find('a', {'class': 'c-card-list__link'}).text.strip()
-            result['time'] = el.find('time', {'class': 'c-card-list__date'}).text.strip()
+
+            try:
+                result['title'] = el.find('a', {'class': 'c-card-list__link'}).text.strip()
+                result['time'] = el.find('time', {'class': 'c-card-list__date'}).text.strip()
+            except AttributeError as e:
+                print(f"Error: {e}")
+                continue
+
             it.append(result)
-        # print(result)
+
+    # Check if any news found
+    if not it:
+        return render(request, 'error.html', {'message': 'No news found.'})
+
     return render(request, 'scrape_it.html', {'it': it})
 
 
@@ -94,17 +187,41 @@ def get_it(request):
 def get_fashion(request):
     fashion = []
     base_url = "https://life.nv.ua/ukr/krasota-i-moda.html"
-    response = requests.get(base_url)
+
+    # Get response from server
+    try:
+        response = requests.get(base_url)
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return render(request, 'error.html', {'message': 'Could not get response from server.'})
+
     soup = BeautifulSoup(response.text, 'html.parser')
-    content = soup.select('div[class="row atom-list"] div[class="atom atom-style col-sm-6 col-lg-4"]')
-    # print(content)
+
+    # Parse content
+    try:
+        content = soup.select('div[class="row atom-list"] div[class="atom atom-style col-sm-6 col-lg-4"]')
+    except AttributeError as e:
+        print(f"Error: {e}")
+        return render(request, 'error.html', {'message': 'Could not parse content.'})
+
+    # Get news items
     for el in content:
         result = {}
-        result['tag'] = el.find('span', {'class': 'atom-additional-category keep-mob'}).text.strip()
-        result['time'] = el.find('span', {'class': 'atom-additional-pub-date'}).text.strip()
-        result['title'] = el.find('div', {'class': 'text'}).text.replace(u'\xa0', u' ')
+
+        try:
+            result['tag'] = el.find('span', {'class': 'atom-additional-category keep-mob'}).text.strip()
+            result['time'] = el.find('span', {'class': 'atom-additional-pub-date'}).text.strip()
+            result['title'] = el.find('div', {'class': 'text'}).text.replace(u'\xa0', u' ')
+        except AttributeError as e:
+            print(f"Error: {e}")
+            continue
+
         fashion.append(result)
-        # print(result)
+
+    # Check if any news found
+    if not fashion:
+        return render(request, 'error.html', {'message': 'No news found.'})
+
     return render(request, 'fashion.html', {'fashion': fashion})
 
 
@@ -112,16 +229,39 @@ def get_fashion(request):
 def get_books(request):
     books = []
     base_url = "https://www.goodreads.com/list/show/2681.Time_Magazine_s_All_Time_100_Novels"
-    response = requests.get(base_url)
+
+    # Get response from server
+    try:
+        response = requests.get(base_url)
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return render(request, 'error.html', {'message': 'Could not get response from server.'})
+
     soup = BeautifulSoup(response.text, 'html.parser')
-    content = soup.select('table[class="tableList js-dataTooltip"] tr')
-    # print(content)
+
+    # Parse content
+    try:
+        content = soup.select('table[class="tableList js-dataTooltip"] tr')
+    except AttributeError as e:
+        print(f"Error: {e}")
+        return render(request, 'error.html', {'message': 'Could not parse content.'})
+
+    # Get news items
     for el in content:
         result = {}
-        result['number'] = el.find('td', {'class': 'number'}).text.strip()
-        result['author'] = el.find('a', {'class': 'authorName'}).text.strip()
-        result['title'] = el.find('a', {'class': 'bookTitle'}).text.strip()
-        result['rating'] = el.find('span', {'class': 'minirating'}).text.strip()
+
+        try:
+            result['number'] = el.find('td', {'class': 'number'}).text.strip()
+            result['author'] = el.find('a', {'class': 'authorName'}).text.strip()
+            result['title'] = el.find('a', {'class': 'bookTitle'}).text.strip()
+            result['rating'] = el.find('span', {'class': 'minirating'}).text.strip()
+        except AttributeError as e:
+            print(f"Error: {e}")
+            continue
+
         books.append(result)
-        # print(result)
+    # Check if any news found
+    if not books:
+        return render(request, 'error.html', {'message': 'No news found.'})
+
     return render(request, 'books.html', {'books': books})
